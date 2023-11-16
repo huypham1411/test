274eba543f5a7decf41545118952feebc89cd540
unknown <Admin@Admin.innotech-vn.com>
2023-11-16 15:56:46 +0700

    improve image

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,21 +12,37 @@ type Props = {};
 const Banner = (props: Props) => {
   return (
     <div className="relative">
-      <Image src={bannerBg} alt="banner-bg" className="w-full h-full" />
+      <Image
+        src={bannerBg}
+        alt="banner-bg"
+        className="w-full h-full"
+        priority
+      />
       <Image
         src={sideBanner}
         alt="side-banner"
         className="w-full h-full mt-[-12px]"
+        priority
       />
       <div className="absolute w-[60%] left-[7.7%] top-[9%]">
         <Image src={bannerTitle} alt="banner-title" className="w-full h-full" />
       </div>
       <div className="absolute top-0 right-[8%] w-[26.5%]">
         <div className="relative">
-          <Image className="w-full h-full" src={mainBanner} alt="main-img" />
+          <Image
+            className="w-full h-full"
+            src={mainBanner}
+            alt="main-img"
+            priority
+          />
           <div className="absolute top-[70%]">
             <div className="relative w-[108%] flex items-center">
-              <Image src={bgTitle} alt="bg-title" className="w-full h-full" />
+              <Image
+                src={bgTitle}
+                alt="bg-title"
+                className="w-full h-full"
+                priority
+              />
               <div className="absolute w-full leading-[112%] text-[120%] md:text-[220%] lg:text-[320%] font-[700] text-white text-center pr-[20%]">
                 THE DJ
               </div>
